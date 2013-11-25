82dbb0043b40862c9bcdda5470548645d9035738
Bernard Maccari <bernard.maccari@epfl.ch>
2013-11-25 18:00:53 +0100

    modified definition of robust angle in isRobust.m

--- Code/isRobust.m
+++ Code/isRobust.m
@@ -1,14 +1,14 @@
 function [ b ] = isRobust( djk, dkl, dlj, dmin )
-% Returns true if the triangle whose edges are djk,dkl,dlj
-% has only angles superior to dmin	  
+% Returns true if distance_min*(sin(angle_min))^2 > dmin  
 
 %compute angles
 angles=[acos((-djk.^2 + dkl.^2 + dlj.^2) / (2*dkl*dlj));
 		acos((+djk.^2 - dkl.^2 + dlj.^2) / (2*djk*dlj));
-		acos((+djk.^2 + dkl.^2 - dlj.^2) / (2*djk*dkl))]
+		acos((+djk.^2 + dkl.^2 - dlj.^2) / (2*djk*dkl))];
 
-%check for angle smaller than dmin
-b=(min(angles) > dmin)
+distances=[djk, dkl, dlj];
+
+b=(min(distances)*(sin(min(angles)).^2) > dmin);
 
 end
 
