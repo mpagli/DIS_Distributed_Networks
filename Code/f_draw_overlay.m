2765233bafbeddb3e9e2e7765ad891c7a8f3f16f
Laurent Fasnacht <l@libres.ch>
2013-12-10 13:59:34 +0100

    doc

--- Code/f_draw_overlay.m
+++ Code/f_draw_overlay.m
@@ -1,13 +1,15 @@
-% Maintainer: Laurent
+function f_draw_overlay(fax, net, edges, color, linewidth)
+% f_draw_overlay(fax, net, edges, color, linewidth)
+%
 % Draw an overlay on edges. This may be used to display current communication, network properties, etc.
+
 % Parameters:
 % - fax: figure axis handle
 % - net: network
 % - edges: K x 2 matrix, with each row is the beginning node id, and the end node id
 % - color: color of an edge
-% - linewidth: widh of an edge
+% - linewidth: width of an edge
 
-function f_draw_overlay(fax, net, edges, color, linewidth)
     N = size(edges,1);
     
     for i=1:N
@@ -16,5 +18,4 @@ function f_draw_overlay(fax, net, edges, color, linewidth)
         li=line([net.location(startnode,1) net.location(endnode,1)], [net.location(startnode,2) net.location(endnode,2)]);
         set(li,'color', color, 'linewidth', linewidth);
     end
-    %scatter(net.location(cn,1),net.location(cn,2),25,'r','filled');
 end
