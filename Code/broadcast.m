f657ebc326d9452cb3ad62f3d610d6b0e4a45c94
Laurent Fasnacht <l@libres.ch>
2013-12-10 13:51:26 +0100

    doc

--- Code/broadcast.m
+++ Code/broadcast.m
@@ -1,6 +1,17 @@
-% Put the message in the output queue of the node
-
 function [node] = broadcast(node, message_type, message_data)
+% [node] = broadcast(node, message_type, message_data)
+% 
+% Broadcast a message to all neighboring nodes
+% This will put the message in the outbox, it will effectively sent by the simulator after all timer_ticks are run.
+%
+% Parameters:
+%  - node: sender node
+%  - message_type: string describing the type of message (one of 'path_length','position','distances','robustquads')
+%  - message_data: matrix of the content of the message (size depends on the type)
+%
+% Output value:
+%  - node: sender node, with the message in the outbox
+
 node.outbox.add(message_type);
 node.outbox.add(message_data);
 return
