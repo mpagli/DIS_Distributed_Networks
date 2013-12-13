d5d88d6d26e3d4a4af5b0dcfa4c6bc59f098d9bb
Laurent Fasnacht <l@libres.ch>
2013-12-13 14:27:18 +0100

    MM rapport

---
+++ Rapport.mm
@@ -0,0 +1,39 @@
+<map version="0.8.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1386938903205" ID="Freemind_Link_1326835076" MODIFIED="1386938923395" TEXT="Rapport">
+<node CREATED="1386938935782" ID="_" MODIFIED="1386938938196" POSITION="right" TEXT="Abstract">
+<node CREATED="1386939815189" ID="Freemind_Link_56161501" MODIFIED="1386939826693" TEXT="We present an implementation of Moore Teller..."/>
+<node CREATED="1386939857300" ID="Freemind_Link_422168503" MODIFIED="1386939861040" TEXT="We evaluate..."/>
+<node CREATED="1386939861556" ID="Freemind_Link_308420388" MODIFIED="1386939878115" TEXT="We show that we get good convergence ..."/>
+</node>
+<node CREATED="1386938938756" ID="Freemind_Link_519501048" MODIFIED="1386938940078" POSITION="right" TEXT="Intro">
+<node CREATED="1386939891617" ID="Freemind_Link_735277680" MODIFIED="1386939900995" TEXT="Localization problem"/>
+<node CREATED="1386939901634" ID="Freemind_Link_1809017818" MODIFIED="1386940010543" TEXT="Distributed (no privileged nodes, same code for all)"/>
+<node CREATED="1386939903708" ID="Freemind_Link_107106976" MODIFIED="1386939906938" TEXT="MooreTeller"/>
+</node>
+<node CREATED="1386938940692" ID="Freemind_Link_328626030" MODIFIED="1386938948089" POSITION="right" TEXT="Experiments">
+<node CREATED="1386939150073" ID="Freemind_Link_1836064661" MODIFIED="1386939197932" TEXT="Modifications over initial paper">
+<node CREATED="1386939268178" ID="Freemind_Link_953570539" MODIFIED="1386939284167" TEXT="Robust quads impl. in O(m&#xb3;)"/>
+<node CREATED="1386939238982" ID="Freemind_Link_777931859" MODIFIED="1386939243218" TEXT="variance estimation"/>
+<node CREATED="1386939184269" ID="Freemind_Link_989300766" MODIFIED="1386939218613" TEXT="sigma ~ edge length"/>
+<node CREATED="1386939299462" ID="Freemind_Link_1146482483" MODIFIED="1386939305213" TEXT="Spring relaxation"/>
+<node CREATED="1386939243734" ID="Freemind_Link_103257254" MODIFIED="1386939262503" TEXT="anchor node estimation + relative positioning only"/>
+</node>
+<node CREATED="1386939311585" ID="Freemind_Link_1626023426" MODIFIED="1386939329620" TEXT="Implementation choices">
+<node CREATED="1386939325819" ID="Freemind_Link_478302540" MODIFIED="1386939349802" TEXT="Fully distributed (private node information not accessible to other without communication)"/>
+<node CREATED="1386939462857" ID="Freemind_Link_1952182748" MODIFIED="1386939529182" TEXT="each simulation timestep in two separate phases: timer tick on nodes (computation), then communication (no computation)"/>
+</node>
+<node CREATED="1386939545996" ID="Freemind_Link_888627207" MODIFIED="1386939581446" TEXT="List parameters"/>
+<node CREATED="1386940710905" ID="Freemind_Link_1611155547" MODIFIED="1386940717767" TEXT="&quot;Regular&quot; graphs"/>
+</node>
+<node CREATED="1386938948445" ID="Freemind_Link_228630638" MODIFIED="1386938955241" POSITION="right" TEXT="Results">
+<node CREATED="1386940524635" HGAP="26" ID="Freemind_Link_1584833169" MODIFIED="1386940538215" TEXT="Plots (L, sigma_*)" VSHIFT="-12"/>
+<node CREATED="1386940516286" ID="Freemind_Link_1129344507" MODIFIED="1386940524056" TEXT="Causes of non-convergence"/>
+</node>
+<node CREATED="1386938955613" ID="Freemind_Link_395047791" MODIFIED="1386938959915" POSITION="right" TEXT="Conclusion"/>
+<node CREATED="1386938960212" ID="Freemind_Link_1650950651" MODIFIED="1386938964440" POSITION="right" TEXT="References">
+<node CREATED="1386938965033" ID="Freemind_Link_1059670575" MODIFIED="1386938967947" TEXT="Moore Teller"/>
+<node CREATED="1386938970354" ID="Freemind_Link_358708153" MODIFIED="1386938972898" TEXT="Knuth"/>
+</node>
+</node>
+</map>
